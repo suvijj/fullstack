e6f1d114b7981819571a0bece25d5d2b0dbf40a9
suvijj <suvi.jylha@outlook.com>
2023-02-21 14:47:41 +0200

    puhelinluettelotehtävän aloitus

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,10 +1,30 @@
 import { useState } from 'react'
+import Contacts from './Contacts'
+import Number from './Contacts'
 
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
-  const [newName, setNewName] = useState('')
+  const [newName, setNewName] = useState(
+    'new contact'
+    )
+
+  const handleAddPerson = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      content: setPersons,
+      id: persons.length + 1,
+    }
+
+    setNewName(newName.concat(personObject))
+    setNewName('')
+  }
 
   return (
     <div>
@@ -14,11 +34,20 @@ const App = () => {
           name: <input />
         </div>
         <div>
+          <form onSubmit={addPerson}>
+          <input
+          value={setNewName}
+          onChange={handleAddPerson}/>
           <button type="submit">add</button>
+          </form>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+      {Contacts.map(contact => 
+          <Contacts key={contact.id} contact={contact} />
+        )}
+      </ul>
     </div>
   )
 
