807e382f0e2cc5dad8c65e1dd7293427bcb09ab6
suvijj <suvi.jylha@outlook.com>
2023-03-03 11:02:11 +0200

    puhelinluettelo osa 2.7-2.8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,31 +1,53 @@
 import {useState} from 'react'
 import Person from './components/Person'
+import Number from './components/Number'
 
-//jääty 2.7
+//alert ei toimi?
 
 
 const App = (props) => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
-
-  const addPerson = (event) => {
-    event.preventDefault()
-    if(persons.find((person) => newName === person.name)) {
-      alert("${newName} is already added to the phonebook");
-    } 
-    const contactObject = {
-      content: newName,
-      name: persons.name
-    }
-   
-    setPersons(persons.concat(contactObject))
-    setNewName('') 
-  }
+  const [newNumber, setNewNumber] = useState('')
+  
 
   const handlePersonChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+    
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newContact = {
+      name: newName,
+      number: newNumber
+    }
+    
+    let matchPerson = null;
+    
+    for(const person of persons) {
+      if(person.name === newName) {
+        matchPerson = person;
+        break;
+      }
+    }
+    if(matchPerson !==null) {
+      alert("This contact is already added to phonebook");
+    }
+     else {
+    setPersons(persons.concat(newContact))
+    setNewName('')
+    setNewNumber('')
+    }
+
+  }
+
+  
   
   return (
     <div>
@@ -36,14 +58,19 @@ const App = (props) => {
            <input type="text" value={newName} onChange={handlePersonChange}/>
           </label>
         </div>
+          <label> number:
+            <input type="number" value={newNumber} onChange={handleNumberChange}/>
+          </label>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => 
-          <Person key={person.name} person={person} />
-        )}
+      {persons.map(person =>
+           <div>
+           <p> {person.name} {person.number}</p>
+          </div>
+        )} 
     </div>
   )
 
---
+++ osa2/puhelinluettelo/src/components/Number.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Number = ({number}) => {
+    return (
+        <li>{number.content}</li>
+    )
+  }
+
+export default Number
